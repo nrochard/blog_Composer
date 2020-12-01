779d389654c48e940859dbbcb654735edd0e3c40
Noémie ROCHARD <nrochard@student.42.fr>
2020-12-01 19:17:58 +0100

    change script composer.json

--- views/posts/form.php
+++ views/posts/form.php
@@ -11,8 +11,7 @@
                 <?php
                 if(isset($_SESSION['messages'])): ?>
                     <div class="alert alert-danger mt-4" role="alert">
-                        <?php 
-                        foreach($_SESSION['messages'] as $message): ?>
+                        <?php foreach($_SESSION['messages'] as $message): ?>
                             <?= $message ?><br>
                         <?php endforeach; ?>
                     </div>
